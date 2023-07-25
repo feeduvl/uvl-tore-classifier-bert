5c9f211e0e7b4753d114612f0f8710f1f8d7fcf9
Lukas Bockstaller <lukas.bockstalller@everest-erp.com>
2023-07-25 11:04:42 +0200

    reconfigure experiments

---
+++ src/pipelines/sner_experiments.sh
@@ -0,0 +1,21 @@
+retry() {
+
+  local command="$1" # Second argument
+
+  # Run the command, and save the exit code
+  $command
+  local exit_code=$?
+
+  # If the exit code is non-zero (i.e. command failed), and we have not
+  # reached the maximum number of retries, run the command again
+  if [[ $exit_code -ne 0 ]]; then
+    retry "$command"
+  else
+    # Return the exit code from the command
+    return $exit_code
+  fi
+}
+
+
+retry "timeout --foreground 1h python src/pipelines/sner_pipeline.py -cp experiments/ -cn 1_stage_sner_parameter_sweep -cd src/pipelines/conf/ -m"
+retry "timeout --foreground 1h python src/pipelines/sner_pipeline.py -cp experiments/ -cn e2e_sner_parameter_sweep -cd src/pipelines/conf/ -m"
